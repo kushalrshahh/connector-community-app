76c18b64b10326eb6d22a5801be0edac48aec62b
Aarav Makadia <52288995+theaaravmakadia@users.noreply.github.com>
2020-03-28 18:18:58 -0400

    Add files via upload
    added name input boxes for volunteer.js

--- src/pages/sign-up/volunteer.js
+++ src/pages/sign-up/volunteer.js
@@ -2,11 +2,13 @@ import React from "react"
 
 //Components
 import Layout from "../../components/layout"
-
+import VolunteerText from "../../components/forms/volunteer-text"
+import VolunteerName from "../../components/forms/volunteer-name"
 
 const VolunteerForm = () => (
   <Layout pageClass="home">
-     volunteer
+    <VolunteerText/>
+    <VolunteerName/>
   </Layout>
 )
 
