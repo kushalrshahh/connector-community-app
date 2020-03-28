71e26b7559a79b71d18c53d2edacd63d3600012b
Jared Shepherd <jared@kaiden.co>
2020-03-28 03:33:24 -0700

    modify header, serve logo image from within directory

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,45 +1,44 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-// import CompanyLogo from '/Users/kushalshah/Documents/connector-community-app/CC-Wordmark_color.png';
-// import CompanyName from '/Users/kushalshah/Documents/connector-community-app/covid connector.png'
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
 
+//Components
+import Menu from "./menu"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
 
+//Stylesheets
+import "../styles/components/header.scss"
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    color: "#F2F2F2"
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "cc-logo-mark.png" }) {
+        childImageSharp {
+          fixed(height: 40) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
   return (
-    <div className={classes.root}>
-      <AppBar position="absolute" color='transparent'>
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-           {/* <img src={CompanyLogo} alt="Logo"/>
-           <img src={CompanyName} alt="Community Connector"/> */}
-          </Typography>
-          <Button color="black">About</Button>
-          <Button color="black">LOGIN</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <header>
+      <div className="content row">
+        <div className="col-5">
+          <div className="logo">
+            <Link to="/">
+              <Img
+                fixed={data.file.childImageSharp.fixed}
+                alt="COVID Connector Logo"
+              />
+              <h2 className="site-name">COVID Connector</h2>
+            </Link>
+          </div>
+        </div>
+        <div className="col-7">
+          <Menu />
+        </div>
+      </div>
+    </header>
   );
 }
