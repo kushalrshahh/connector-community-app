d796b5d13bfc84eb52cc732b7b33e0b96cfd833c
Jared Shepherd <jared@kaiden.co>
2020-03-28 01:45:14 -0700

    participating cities intial layout

---
+++ src/components/pages/home/participating-cities.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+//Stylesheets
+import "../../../styles/components/pages/home/participating-cities.scss"
+
+export default () => (
+  <section className="participating-cities">
+    <div className="content">
+      Participating Cities
+    </div>
+  </section>
+)
