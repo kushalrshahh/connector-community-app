2dd01412622ff23c2f117ce6647f99a7a30675e2
Jared Shepherd <jared@kaiden.co>
2020-03-28 00:56:25 -0700

    layout component

---
+++ src/components/layout.js
@@ -0,0 +1,32 @@
+ //Dependencies
+ import React from "react"
+ import { useStaticQuery, graphql } from "gatsby"
+
+ //Components
+ import Header from "./AppBar"
+ import Footer from "./footer"
+
+ //Stylesheets
+ import "../styles/utilities/bootstrap-grid.scss"
+ import "../styles/components/layout.scss"
+ import "../styles/components/forms/forms.scss"
+ 
+ export default (props) => {
+   const data = useStaticQuery(graphql`
+     query SiteTitleQuery {
+       site {
+         siteMetadata {
+           title
+         }
+       }
+     }
+   `)
+   return (
+     <article className={props.pageClass}>
+       <Header siteTitle={data.site.siteMetadata.title} />
+       <main>{props.children}</main>
+       <Footer />  
+     </article>
+   )
+ }
+ 
