4633adc3db47ca3ace73e071483ed75e629fcede
Jared Shepherd <jared@kaiden.co>
2020-03-27 21:10:30 -0700

    add support for typography, meta data, analytics, strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,65 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `CommunityConnector`,
+    description: `Connecting those that need help with those that are able and willing to provide it during the COVID pandemic.`,
+    author: `CommunityConnector Team`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        contentTypes: [
+          `event`,
+          `user`
+        ],
+        queryLimit: 1000,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "covid-connector.com",
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,29 @@
+import Typography from "typography"
+
+const typography = new Typography(
+  {
+    scaleRatio: 1.75,
+    googleFonts: [
+      {
+        name: 'Open Sans',
+        styles: [
+          '400',
+          '600'
+        ],
+      },
+      {
+        name: 'Raleway',
+        styles: [
+          '600',
+          '800'
+        ],
+      }
+    ],
+    headerFontFamily: ['Raleway'],
+    bodyFontFamily: ['Open Sans']
+  }
+)
+
+// Export helper functions
+export const { scale, rhythm, options } = typography
+export default typography
