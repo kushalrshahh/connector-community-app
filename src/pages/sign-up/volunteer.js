8eb66a34a3d6266824c75e0bc2403698683ae95e
Keerthan Vantakala <keerthanvantakala@gmail.com>
2020-03-28 21:44:24 -0500

    not working

--- src/pages/sign-up/volunteer.js
+++ src/pages/sign-up/volunteer.js
@@ -8,14 +8,13 @@ import VolunteerInfo from "../../components/pages/forms/volunteer-info"
 
 export default class VolunteerForm extends  React.Component {
 
-
-
   render () {
+
     return (
       <Layout pageClass="home">
       <div>
   
-  
+
       <VolunteerText/>
       <form>
         <ContactInfo/>
