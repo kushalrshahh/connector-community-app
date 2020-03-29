1b62545ccb4ad99d3e56c66bbc08925c6ea4f168
Keerthan Vantakala <keerthanvantakala@gmail.com>
2020-03-28 22:20:21 -0500

    combine

--- src/components/pages/forms/volunteer-info.js
+++ src/components/pages/forms/volunteer-info.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import Input from "../../forms/input"
 import Radio from "../../forms/radio"
-
+import Button from "../../forms/button"
+import Checkbox from "../../forms/checkbox"
 
 //Stylesheets
 import "../../../styles/components/pages/forms/volunteer-info.scss"
 
 export default class VolunteerInfo extends React.Component {
+    state = {
+        firstName: "",
+        lastName: "",
+      }
+    handleInputChange = event => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+          [name]: value,
+        })
+      }
 
     render() {
       return (
           <>
+        <form>
+        <div>
+          <Input type="text" label="Name" name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleInputChange}/>
+          <Input type="text"  name="lastName" placeholder="Last Name"/><br></br>
+          <Checkbox name="chkName" id="checkName" label="If you are comfortable sharing your name with the ones you help, please check the box"/>
+          <Input type="text" label="Alias" name="alias" placeholder="Alias" maxlength="10"/><br></br>
+          <Input type="tel" label="Phone Number" name="phone1" maxlength="3"/>
+          <Input type="tel" name="phone2" maxlength="3"/>
+          <Input type="tel" name="phone3" maxlength="4"/>
+          <Input type="email" label="Email" name="email" placeholder="Email"/><br></br>
+        </div>
         <div className="row no-gutters">
             <div className="col-12 col-md">
                 <li class="horiz"> 
@@ -27,7 +51,8 @@ export default class VolunteerInfo extends React.Component {
                 <Input type='text' label='What would you like to help with?' name="helpme" placeholder="things you are willing to do. eg. buying groceries or driving to the doctor"/>
             </div>
         </div>
-
+        <Button to="#">Submit</Button>
+        </form>
         </>
       )
     }
--- src/pages/sign-up/volunteer.js
+++ src/pages/sign-up/volunteer.js
@@ -3,7 +3,6 @@ import React from "react"
 //Components
 import Layout from "../../components/layout"
 import VolunteerText from "../../components/pages/forms/volunteer-text"
-import ContactInfo from "../../components/pages/forms/contact-info"
 import VolunteerInfo from "../../components/pages/forms/volunteer-info"
 
 export default class VolunteerForm extends  React.Component {
@@ -16,11 +15,9 @@ export default class VolunteerForm extends  React.Component {
   
 
       <VolunteerText/>
-      <form>
-        <ContactInfo/>
+
         <VolunteerInfo/>
-      <button type="submit">Submit</button>
-      </form>
+
       </div>
     </Layout>
     )
