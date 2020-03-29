6a5bd7907dd26d0ce2d5887105af564d6a04531b
Keerthan Vantakala <keerthanvantakala@gmail.com>
2020-03-28 22:07:59 -0500

    add aa work

--- src/components/pages/forms/contact-info.js
+++ src/components/pages/forms/contact-info.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react'
 import Input from "../../forms/input"
+import Check from "../../forms/checkbox"
 //Stylesheets
 
 
-class VolunteerName extends Component {
+export default class VolunteerName extends React.Component {
 
- 
     render() {
-      const { firstName , lastName } = this.props;
-      //create constructor 
       return (
-       <div>
-          <Input type="text" label="Name" name="firstName" placeholder="First Name" value={firstName}/>
-          <Input type="text"  name="lastName" placeholder="Last Name" value={lastName}/><br></br>
-          <Input type="checkbox" label="If you are comfortable sharing your name with the ones you help, please check the box" name="chkName" id="checkName"/>
+        <form>
+          <Input type="text" label="Name" name="firstName" placeholder="First Name"/>
+          <Input type="text"  name="lastName" placeholder="Last Name"/><br></br>
+          <Check type="checkbox" name="chkName" id="checkName" label="If you are comfortable sharing your name with the ones you help, please check the box"/>
           <Input type="text" label="Alias" name="alias" placeholder="Alias" maxlength="10"/><br></br>
           <Input type="tel" label="Phone Number" name="phone1" maxlength="3"/>
           <Input type="tel" name="phone2" maxlength="3"/>
           <Input type="tel" name="phone3" maxlength="4"/>
           <Input type="email" label="Email" name="email" placeholder="Email"/><br></br>
-        </div>
-      );
+        </form>
+      )
     }
-    }
-
-
-export default VolunteerName;
+}
