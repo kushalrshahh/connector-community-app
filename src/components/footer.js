973c64591d7ce29d671d1d060c3c578626b05b09
Jared Shepherd <jared@kaiden.co>
2020-03-29 20:32:52 -0700

    footer basic layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,25 @@
 import React from "react"
 
+//Components
+import Icon from "../components/shared/icon"
+import LogoMark from "../images/icons/cc-logomark-colored.svg"
+
 //Stylesheets
 import "../styles/components/footer.scss"
 
 export default (props) => {
   return (
     <footer>
-      <div className="content row justify-content-center">
-        <div className="col-4">
-          Logo
+      <div className="content row">
+        <div className="col-4 logo-mark">
+          <Icon>
+            <LogoMark/>
+          </Icon>
         </div>
         <div className="col-8">
-          <p>Need help?</p>
-          <p><a href="mailto:info@covid-connectore.com">{props.support}</a></p>
-          <p>{props.copyright}</p>
+          <p className="help">Need help?</p>
+          <p><a className="support-email" href="mailto:info@covid-connectore.com">{props.support}</a></p>
+          <p className="copyright">{props.copyright}</p>
         </div>
       </div>
     </footer>
