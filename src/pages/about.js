ec73e1004a06dac0103a1741085e6690faab298f
Jared Shepherd <jared@kaiden.co>
2020-03-29 19:24:49 -0700

    add page meta

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import {Helmet} from "react-helmet"
 
 //Components
 import Layout from "../components/layout"
@@ -6,6 +7,11 @@ import Layout from "../components/layout"
 export default function Profile() {
   return (
     <Layout pageClass="about-us">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>About | COVID Connector</title>
+        <link rel="canonical" href="https://www.covid-connector.com" />
+      </Helmet>
       <div className="content">
         <p>About Us</p>
       </div>
