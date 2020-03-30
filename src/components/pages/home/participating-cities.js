f4b3c2cb7134bd38eb57d110bf2b4a10e29fc031
Jared Shepherd <jared@kaiden.co>
2020-03-29 19:43:29 -0700

    abbreviate cities on mobile

--- src/components/pages/home/participating-cities.js
+++ src/components/pages/home/participating-cities.js
@@ -6,7 +6,15 @@ import "../../../styles/components/pages/home/participating-cities.scss"
 export default () => (
   <section className="participating-cities">
     <div className="content">
-      SAN FRANCISO<span className="city-split">|</span>SEATTLE<span className="city-split">|</span>NEW YORK
+      <div className="city">
+        S<span className="mobile-hide">AN&nbsp;</span>F<span className="mobile-hide">RANCISO&nbsp;</span>
+      </div>
+      <div className="city">
+        SEA<span className="mobile-hide">TTLE&nbsp;</span>
+      </div>
+      <div className="city">
+        N<span className="mobile-hide">EW&nbsp;</span>Y<span className="mobile-hide">ORK&nbsp;</span>
+      </div>
     </div>
   </section>
 )
